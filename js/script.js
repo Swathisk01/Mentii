8fe8efe4c15b5836f89f67d37a43d2e8190c5e75
Swathisk01 <swathiskumar01@gmail.com>
2024-04-06 19:01:27 +0530

    New Changes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,106 @@
-const nav = document.querySelector(".nav"),
-  searchIcon = document.querySelector("#searchIcon"),
-  navOpenBtn = document.querySelector(".navOpenBtn"),
-  navCloseBtn = document.querySelector(".navCloseBtn");
+function homePageAnimation(){
+  gsap.set(".slidesm", {scale: 5})
 
-searchIcon.addEventListener("click", () => {
-  nav.classList.toggle("openSearch");
-  nav.classList.remove("openNav");
-  if (nav.classList.contains("openSearch")) {
-    return searchIcon.classList.replace("uil-search", "uil-times");
-  }
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
+var tl = gsap. timeline({
+scrollTrigger: {
+trigger: ".home",
+start: "top top",
+end: "bottom bottom",
+scrub: 1
+}
+})
 
-navOpenBtn.addEventListener("click", () => {
-  nav.classList.add("openNav");
-  nav.classList.remove("openSearch");
-  searchIcon.classList.replace("uil-times", "uil-search");
-});
-navCloseBtn.addEventListener("click", () => {
-  nav.classList.remove("openNav");
-});
+tl
+.to(".vdodiv", {
+'--clip': "0%",
+ease: Power2
+}, 'a')
+
+.to(".slidesm", {
+  scale: 1,
+  ease: Power2
+  }, 'a')
+
+.to(".lft", {
+xPercent: -10,
+stagger: .03,
+ease: Power4
+}, 'b')
+
+.to(".rgt", {
+xPercent: 10,
+stagger: .03,
+ease: Power4
+}, 'b')
+}
+
+function realAnimation(){
+  gsap.to(".slide",{
+      scrollTrigger: {
+          trigger: ".real",
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 1
+      },
+      xPercent: -200,
+      ease: Power4
+  })
+}
+
+function teamAnimation()
+{
+  document.querySelectorAll(".listelem")
+.forEach(function(el){
+  el.addEventListener("mousemove", function(dets){
+      
+     gsap.to(this.querySelector(".picture"), 
+     {
+      opacity: 1,
+      x: gsap.utils.mapRange(0, window.innerWidth,-200,200, dets.clientX),
+      ease: Power4, 
+      duration: .5
+     })
+  })
+  el.addEventListener("mouseleave", function(dets){
+      gsap.to(this.querySelector(".picture"), 
+      {
+      opacity: 0 ,
+      ease: Power4,
+      duration: .5
+      })
+  })
+
+
+})
+}
+
+function backgroundcolor()
+{
+  
+document.querySelectorAll(".section")
+.forEach(function(e) {
+ScrollTrigger.create({
+trigger: e,
+start: "top 50%",
+end: "bottom 50%",
+onEnter: function () {
+  document.body.setAttribute("theme", e.dataset.color);
+},
+onEnterBack: function () {
+  document.body.setAttribute("theme", e.dataset.color);
+}
+})
+})
+
+}
+
+
+
+(function () {
+  const locomotiveScroll = new LocomotiveScroll();
+})();
+
+homePageAnimation();
+realAnimation();
+teamAnimation();
+backgroundcolor();
